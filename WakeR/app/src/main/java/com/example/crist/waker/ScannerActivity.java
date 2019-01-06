0c7a4c633bdfbf4f1d0d36e91211f056d8e82a3c
Unknown <cristian.baba09@yahoo.com>
2019-01-06 20:20:51 +0100

    Qr scanner works

--- WakeR/app/src/main/java/com/example/crist/waker/ScannerActivity.java
+++ WakeR/app/src/main/java/com/example/crist/waker/ScannerActivity.java
@@ -6,11 +6,24 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ScannerActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ScannerActivity extends AppCompatActivity implements View.OnClickListener {
     private BottomNavigationView mainNav;
     private FrameLayout mainFrame;
+    private Button buttonScan;
+    private TextView textViewName, textViewID;
+    private IntentIntegrator qrScan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,12 @@ public class ScannerActivity extends AppCompatActivity {
         //init all
         mainFrame = (FrameLayout) findViewById(R.id.main_frame);
         mainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+        qrScan = new IntentIntegrator(this);
+        //testing
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        textViewID = (TextView) findViewById(R.id.textViewID);
+        buttonScan.setOnClickListener(this);
 
 
 
@@ -45,5 +64,38 @@ public class ScannerActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    textViewName.setText(obj.getString("name"));
+                    textViewID.setText(obj.getString("personalwakenr"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+qrScan.initiateScan();
     }
 }
